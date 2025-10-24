8be4310044bcc3e028d89cad7e0e5fd019b4f1ed
Mangus001 <ttin51583@gmail.com>
2025-10-24 10:33:26 +0300

    Create 31.py

---
+++ 31.py
@@ -0,0 +1,4 @@
+population = int(input("Введите количество жителей: "))
+area = float(input("Введите площадь территории (в км^2): "))
+density = population / area
+print(f"Плотность населения: {density:.2f} человек на км^2")
