e413f10f5e9e27ccc604e34eae937eb1a78d5508
Mangus001 <ttin51583@gmail.com>
2025-10-24 10:21:53 +0300

    Create  26.py

---
+++ 26.py
@@ -0,0 +1,8 @@
+a = float(input("Введите длину ребра куба: "))
+
+volume = a ** 3
+
+lateral_area = 4 * (a ** 2)
+
+print(f"Объем куба: {volume}")
+print(f"Площадь боковой поверхности: {lateral_area}")
