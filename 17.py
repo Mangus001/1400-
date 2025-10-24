367f10c029332de3edd4b9269a48b5adb6c91f36
Mangus001 <ttin51583@gmail.com>
2025-10-24 10:13:56 +0300

    Create 17.py

---
+++ 17.py
@@ -0,0 +1,9 @@
+a = int(input("Введите значение a: "))
+b = int(input("Введите значение b: "))
+x = int(input("Введите значение x: "))
+y = int(input("Введите значение y: "))
+
+print(f"2 кг")
+print(f"{a} {b} в") 
+print(f"{x} {y}")
+print(f"13 17 19 b 5 y")
