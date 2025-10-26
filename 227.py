9070e8f2055bb85c44d8a6c84567f01df6121408
Mangus001 <ttin51583@gmail.com>
2025-10-26 08:29:39 +0300

    Create 227.py

---
+++ 227.py
@@ -0,0 +1,11 @@
+a, b, c, d = float(input()), float(input()), float(input()), float(input())
+sum_gt_5 = 0
+if a > 5:
+    sum_gt_5 += a
+if b > 5:
+    sum_gt_5 += b
+if c > 5:
+    sum_gt_5 += c
+if d > 5:
+    sum_gt_5 += d
+print(sum_gt_5)
