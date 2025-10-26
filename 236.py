158ed4215a3c8cdfbb97fb52bebdec03b499aa70
Mangus001 <ttin51583@gmail.com>
2025-10-26 08:32:34 +0300

    Create 236.py

---
+++ 236.py
@@ -0,0 +1,8 @@
+mit_age = int(input())
+vasia_age = int(input())
+if mit_age > vasia_age:
+    print("Митше старше")
+elif mit_age < vasia_age:
+    print("Васе старше")
+else:
+    print("Они одного возраста")
