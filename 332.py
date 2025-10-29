d3b0782933100f74037106287a6615d1afa8ed6b
Mangus001 <ttin51583@gmail.com>
2025-10-29 08:18:15 +0300

    Create 332.py

---
+++ 332.py
@@ -0,0 +1,8 @@
+n = int(input("Введите n (2 ≤ n ≤ 10): "))
+sum_factorials = 0
+for k in range(1, n+1):
+    factorial = 1
+    for i in range(1, k+1):
+        factorial *= i
+    sum_factorials += factorial
+print(f"Сумма факториалов: {sum_factorials}")
