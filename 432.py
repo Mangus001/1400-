ad05d6a18ee3bc79a0e251178ae708558914decc
Mangus001 <ttin51583@gmail.com>
2025-12-16 09:37:51 +0300

    Create 432.py

---
+++ 432.py
@@ -0,0 +1,7 @@
+digits = [int(d) for d in number_str]
+
+max_digit = max(digits)
+indices_max = [i for i, d in enumerate(digits) if d == max_digit]
+pos_max_from_start = [indices_max[0], indices_max[-1]] 
+pos_max_from_end = [len(digits) - 1 - indices_max[-1],
+                    len(digits) - 1 - indices_max[0]] 
