4b06fdb1a75f5a07cf788991081b214b03245834
Mangus001 <ttin51583@gmail.com>
2025-12-16 09:32:47 +0300

    Create 422.py

---
+++ 422.py
@@ -0,0 +1,14 @@
+a = '3' 
+b = '5'
+k = 2
+number_str = str(number)
+
+has_a = a in number_str
+
+no_b = b not in number_str
+
+count_a = number_str.count(a)
+more_than_k = count_a > k
+
+
+has_both = (a in number_str) and (b in number_str)
