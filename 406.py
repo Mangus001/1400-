0d1ecf7a0289c5a1eb9e23f6a310605ce201b2bf
Mangus001 <ttin51583@gmail.com>
2025-12-16 09:25:43 +0300

    Create 406.py

---
+++ 406.py
@@ -0,0 +1,8 @@
+n = int(input())
+number_str = str(n)
+pos = 0
+for i in range(len(number_str)-1, -1, -1):
+    if number_str[i] == '8':
+        pos = len(number_str) - i
+        break
+print(pos)
