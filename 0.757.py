ae141eaa9d349de65dad1b0c2eaa8039b85a6cc3
Mangus001 <ttin51583@gmail.com>
2025-12-16 12:21:45 +0300

    Create 0.757.py

---
+++ 0.757.py
@@ -0,0 +1,10 @@
+def unique_prime_factors(n):
+    factors = set()
+    divisor = 2
+    while n > 1:
+        if n % divisor == 0:
+            factors.add(divisor)
+            n //= divisor
+        else:
+            divisor += 1
+    return list(factors)
