3508b282473f4e0e5ad4f0105e69123a2dc2cbaa
Mangus001 <ttin51583@gmail.com>
2025-12-16 09:30:16 +0300

    Create 420.py

---
+++ 420.py
@@ -0,0 +1,11 @@
+n = int(input())
+s = [int(d) for d in str(n)]
+sum_digits = sum(s)
+prod_digits = 1
+for d in s:
+    prod_digits *= d
+length = len(s)
+first_digit = s[0]
+last_digit = s[-1]
+print(sum_digits > k and n % 2 == 0)
+print(length % 2 == 0 and n <= b)
