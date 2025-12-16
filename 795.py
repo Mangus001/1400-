c1797f0121dfa622ed1f553a3242fcfb35bfb3e9
Mangus001 <ttin51583@gmail.com>
2025-12-16 11:58:12 +0300

    Create 795.py

---
+++ 795.py
@@ -0,0 +1,11 @@
+a = int(input("Введите значение a: "))
+b = float(input("Введите значение b: "))
+
+k = random.randint(1, a)
+print(f"Случайное число k: {k}")
+
+print("Случайные числа ni:")
+
+for _ in range(k):
+    ni = random.random() * b
+    print(ni)
