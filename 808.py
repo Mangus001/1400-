8e5a84dc7804fb1d776c3edad5dc56a0d3e964e9
Mangus001 <ttin51583@gmail.com>
2025-12-16 12:11:27 +0300

    Create 808.py

---
+++ 808.py
@@ -0,0 +1,7 @@
+counts = {i:0 for i in range(1,7)}
+for _ in range(100):
+    roll = random.randint(1,6)
+    counts[roll] += 1
+for _ in range(900):
+    roll = random.randint(1,6)
+    counts[roll] += 1
