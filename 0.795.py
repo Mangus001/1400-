83a8b173769a82befe6638fcf1742af76aad4dfc
Mangus001 <ttin51583@gmail.com>
2025-12-16 12:39:23 +0300

    Create 0.795.py

---
+++ 0.795.py
@@ -0,0 +1,7 @@
+a = random.randint(0, 1)
+b = random.randint(0, 2)
+a2 = random.randint(1, 2)
+b2 = random.randint(0, 2)
+c = random.randint(1, 3)
+numbers = [2]*7 + [3]*8
+sample_numbers = random.sample(numbers, 15)
