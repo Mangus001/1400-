d0725ea92909be9d245cc79672b5ccde75ff605d
Mangus001 <ttin51583@gmail.com>
2025-12-16 10:08:43 +0300

    Create 523.py

---
+++ 523.py
@@ -0,0 +1,9 @@
+n_gazeta = int(input())
+n_jurnal = int(input())
+total_pages = 0
+for _ in range(n_gazeta):
+    pages = int(input())
+for _ in range(n_jurnal):
+    pages = int(input())
+    total_pages += pages
+print(total_pages)
