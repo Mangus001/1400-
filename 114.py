e9b40e93fd4d0a189ee3608c4eb142d37463d92d
Mangus001 <ttin51583@gmail.com>
2025-12-16 09:28:20 +0300

    Create 114.py

---
+++ 114.py
@@ -0,0 +1,14 @@
+n = int(input())
+digits = [int(d) for d in str(n)]
+max_digit = max(digits)
+min_digit = min(digits)
+pos_max_end = pos_max_start = pos_min_end = pos_min_start = 0
+
+for i, d in enumerate(digits):
+    if d == max_digit:
+        pos_max_start = i + 1
+        pos_max_end = len(digits) - i
+    if d == min_digit:
+        pos_min_start = i + 1
+        pos_min_end = len(digits) - i
+print(pos_max_end, pos_max_start, pos_min_end, pos_min_start)
