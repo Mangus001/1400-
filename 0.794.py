00a350fa6eab84c47157c110cb26cff26b85d2d7
Mangus001 <ttin51583@gmail.com>
2025-12-16 12:39:01 +0300

    Create 0.794.py

---
+++ 0.794.py
@@ -0,0 +1,12 @@
+m = int(input("Введите значение m: "))
+a = float(input("Введите значение a: "))
+b = float(input("Введите значение b: "))
+
+k = random.randint(1, m)
+print(f"Случайное число k: {k}")
+
+print("Случайные числа ni:")
+
+for _ in range(k):
+    ni = a + random.random() * (b - a)
+    print(ni)
