39b66bf478f9a2bb1e1bd1458a2fb77ab7d19764
Mangus001 <ttin51583@gmail.com>
2025-12-21 17:33:14 +0300

    Create 1197.py

---
+++ 1197.py
@@ -0,0 +1,9 @@
+s = input()
+first_comma = s.find(',')
+second_comma = s.find(',', first_comma + 1)
+if second_comma != -1:
+    print(s[first_comma+1:second_comma])
+elif first_comma != -1:
+    print(s[first_comma+1:])
+else:
+    print(s)
