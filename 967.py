5fd909bda6ced7c67666f4640c157a3a89f742c8
Mangus001 <ttin51583@gmail.com>
2025-12-21 10:23:32 +0300

    Create 967.py

---
+++ 967.py
@@ -0,0 +1,9 @@
+arr = [4, 8, 15, 16, 23, 42]
+arr_doubled = [x*2 for x in arr]
+A = 10
+arr_subtracted = [x - A for x in arr]
+first_elem = arr[0]
+arr_divided = [x/first_elem for x in arr]
+print(arr_doubled)
+print(arr_subtracted)
+print(arr_divided)
