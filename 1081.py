0823c9dc6c893bc54cfe7e62e84762014f50c2ad
Mangus001 <ttin51583@gmail.com>
2025-12-21 11:47:17 +0300

    Update 1081.py

--- 1081.py
+++ 1081.py
@@ -6,4 +6,4 @@ max_length = max(len(w) for w in words)
 shortest_idx = next(i for i,w in enumerate(words) if len(w) == min(len(w) for w in words))
 count_bigger_max = sum(1 for w in words if len(w) > max_length)
 count_k = sum(1 for w in words if w.lower().startswith('к'))
-sorted_words = sorted(words)
+sorted_words = sorted(words
