5846034508187218096607dfbdfc50586b2588a3
Mangus001 <ttin51583@gmail.com>
2025-12-21 10:23:51 +0300

    Create 969.py

---
+++ 969.py
@@ -0,0 +1,8 @@
+B = 3
+last_elem = arr[-1]
+arr_minus_20 = [x - 20 for x in arr]
+arr_mult_last = [x * last_elem for x in arr]
+arr_plus_B = [x + B for x in arr]
+print(arr_minus_20)
+print(arr_mult_last)
+print(arr_plus_B)
