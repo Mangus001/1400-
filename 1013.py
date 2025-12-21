957d3a13db1cd1f5ffa4aef265757465a038274e
Mangus001 <ttin51583@gmail.com>
2025-12-21 11:12:10 +0300

    Create 1013.py

---
+++ 1013.py
@@ -0,0 +1,6 @@
+growths = [int(input()) for _ in range(15)]
+growths = sorted(growths, reverse=True)
+new_growth = int(input())
+import bisect
+pos = bisect.bisect_left(growths, new_growth, hi=len(growths))
+print(pos+1) 
