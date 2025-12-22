8e34ead593ee0ebb5c2cfc91863a348d99262334
Mangus001 <ttin51583@gmail.com>
2025-12-22 16:27:24 +0300

    Update 1400.py

--- 1400.py
+++ 1400.py
@@ -1,7 +1,25 @@
-def simplify_fraction(a, b):
-    divisor = gcd(a, b)
-    return a // divisor, b // divisor
+a = 1
+b = 2
+c = 3
+d = 4
 
-a, b = 8, 12
-p, q = simplify_fraction(a, b)
-print(f"Сокращенная дробь: {p}/{q}")
+expr1 = a / (b * c)
+
+expr2 = a + (b / c)
+
+expr3 = (a + b) / c
+
+expr4 = (a * b) / (c + 2)
+
+expr5 = (a / b) * ((c - 3) / d)
+
+numerator = (a / 3) + (b / 2)
+denominator = b + (c / (2 + b))
+expr6 = numerator / denominator
+
+print("выражение 1:", expr1)
+print("выражение 2:", expr2)
+print("выражение 3:", expr3)
+print("выражение 4:", expr4)
+print("выражение 5:", expr5)
+print("выражение 6:", expr6)
