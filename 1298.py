352ee333d811125ee1d3d95b6fd2e3b52ad642a5
Mangus001 <ttin51583@gmail.com>
2025-12-22 14:46:30 +0300

    Create 1298.py

---
+++ 1298.py
@@ -0,0 +1,13 @@
+ine = input()
+desired_length = int(input())
+words = line.split()
+total_spaces = desired_length - sum(len(w) for w in words)
+gaps = len(words) - 1
+base = total_spaces // gaps
+extra = total_spaces % gaps
+result = ''
+for i, w in enumerate(words):
+    result += w
+    if i < gaps:
+        result += ' ' * (base + (1 if i < extra else 0))
+print(result)
