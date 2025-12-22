32c43b0355231e782872b29fe75bff5d31661f50
Mangus001 <ttin51583@gmail.com>
2025-12-22 15:59:58 +0300

    Update 1372.1.py

--- 1372.1.py
+++ 1372.1.py
@@ -1,4 +1,4 @@
-ef isosceles_trapezoid_perimeter(base1, base2, height):
+def isosceles_trapezoid_perimeter(base1, base2, height):
     side = math.sqrt(((base2 - base1) / 2)**2 + height**2)
     return base1 + base2 + 2 * side
 
