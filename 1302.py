d39930437b8319f3d949d144f43ad2e749f45b5b
Mangus001 <ttin51583@gmail.com>
2025-12-22 15:11:27 +0300

    Create 1302.py

---
+++ 1302.py
@@ -0,0 +1,4 @@
+cities_str = [("Рим", "Италия"), ("Флоренция", "Италия"), ("Милан", "Италия"), ("Лондон", "Англия")]
+for city, country in cities_str:
+    if country == "Италия":
+        print(city)
