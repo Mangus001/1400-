14a551ff9d71f93b42dbb3b5530022b5125a0bae
Mangus001 <ttin51583@gmail.com>
2025-12-22 15:14:57 +0300

    Create 1315.py

---
+++ 1315.py
@@ -0,0 +1,15 @@
+phonebook = [("Иванов", 89051234567), ("Петров", 8905123456)]
+search_phone = 8905123456
+found = False
+for f, p in phonebook:
+    if p == search_phone:
+        print(f)
+        found=True
+if not found:
+    print("Нет")
+# обратная проверка
+found_name = None
+for f, p in phonebook:
+    if p == search_phone:
+        print(f)
+        break
